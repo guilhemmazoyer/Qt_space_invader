59c707f110621442db435500940380f94563168b
guilhemmazoyer <guilhem.mazoyer@laposte.net>
2022-05-13 01:25:38 +0200

    Ajout d'un mouvement fluide quand une touche de déplacement est maintenue enfoncée par le joueur. pas appliqué aux tirs

--- SpaceInvader/player.cpp
+++ SpaceInvader/player.cpp
@@ -1,6 +1,7 @@
 #include "player.h"
 #include "missile.h"
 #include "game.h"
+#include "qevent.h"
 #include <QGraphicsScene>
 #include <QKeyEvent>
 #include <QPixmap>
@@ -10,6 +11,9 @@ extern Game * game;
 Player::Player(){
     setPixmap(QPixmap(":/images/player.png"));
 
+    goLeft = false;
+    goRight = false;
+
     float random_positon = rand() % 20;
     random_positon *= game->sceneWidth/20;
     setPos(random_positon, game->scene->height() - pixmap().height());
@@ -17,31 +21,57 @@ Player::Player(){
 }
 
 void Player::keyPressEvent(QKeyEvent *event){
+    // Move left
+    if ((event->key() == Qt::Key_Q) || (event->key() == Qt::Key_Left)) {
+        goLeft = true;
+    }
+
+    // Move right
+    else if ((event->key() == Qt::Key_D) || (event->key() == Qt::Key_Right)) {
+        goRight = true;
+    }
+
+    else if ((event->key() == Qt::Key_Space)) {
+        // Create a bullet
+        Missile * missile = new Missile();
+        missile->setPos(x() + game->spaceshipWidthOffset, y()-50);
+        scene()->addItem(missile);
+        game->setsound(":/sounds/blaster.wav", 3, 1);
+        game->playerJukebox.play();
+    }
+
+    else if ((event->key() == Qt::Key_Escape)) {
+        // Pause the game
+        game->setsound(":/sounds/click2.wav", 1, 1);
+        game->interfaceJukebox.play();
+        game->displayPause();
+    }
+}
+
+void Player::keyReleaseEvent(QKeyEvent *event){
+    // Move left
+    if ((event->key() == Qt::Key_Q) || (event->key() == Qt::Key_Left)) {
+        goLeft = false;
+    }
+    // Move right
+    else if ((event->key() == Qt::Key_D) || (event->key() == Qt::Key_Right)) {
+        goRight = false;
+    }
+}
+
+void Player::action(){
     if(game->isPauseMenuClosed){
-        // Move left
-        if ((event->key() == Qt::Key_Q) || (event->key() == Qt::Key_Left)) {
+        // go Left
+        if(goLeft){
             if (pos().x() > 0)
                 setPos(x()-(game->sceneWidth/20), y());
         }
-        // Move right
-        else if ((event->key() == Qt::Key_D) || (event->key() == Qt::Key_Right)) {
+
+        // go Right
+        if(goRight){
             if (pos().x() + 150 < (game->sceneWidth)){
                 setPos(x()+(game->sceneWidth/20), y());
             }
-        }
-        else if ((event->key() == Qt::Key_Space)) {
-            // Create a bullet
-            Missile * missile = new Missile();
-            missile->setPos(x() + game->spaceshipWidthOffset, y()-50);
-            scene()->addItem(missile);
-            game->setsound(":/sounds/blaster.wav", 3, 1);
-            game->playerJukebox.play();
-        }
-        else if ((event->key() == Qt::Key_Escape)) {
-            // Pause the game
-            game->setsound(":/sounds/click2.wav", 1, 1);
-            game->interfaceJukebox.play();
-            game->displayPause();
         }
     }
 }
--- SpaceInvader/player.h
+++ SpaceInvader/player.h
@@ -7,13 +7,19 @@
 
 class Player: public QObject, public QGraphicsPixmapItem{
     Q_OBJECT
+
+    bool goLeft;
+    bool goRight;
+
     void keyPressEvent(QKeyEvent * event);
+    void keyReleaseEvent(QKeyEvent * event);
 
 public:
     Player();
 
 public slots:
     void spawn();
+    void action();
 };
 
 #endif // PLAYER_H
